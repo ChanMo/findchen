deff1982a766c6980a9476ed787b38041712945d
dyou <chen.orange@aliyun.com>
2019-09-17 00:07:00 +0800

    change site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `FindChen`,
+    author: `ChanMo`,
+    description: `A simple blog.`,
+    siteUrl: `https://www.findchen.com`,
     social: {
-      twitter: `kylemathews`,
     },
   },
   plugins: [
